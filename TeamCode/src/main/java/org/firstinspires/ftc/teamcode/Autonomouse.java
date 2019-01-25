0e5ef86524fb105fe77305b8a0ee8cfe7da11f34
gerry2022153 <gerry2022153@concordiashanghai.org>
2019-01-25 17:28:48 +0800

    Defined the arm motor as well as the claw servos (the claw servos are commented out because they claw servos haven't been properly installed yet).
    Also, a "state" variable to store the current robot state for easier autonomous control.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomouse.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomouse.java
@@ -3,9 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
 @Autonomous(name="java Linear Autonomous", group="Linear Opmode")
-//@Disabled
 public class Autonomouse extends LinearOpMode {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
@@ -13,6 +14,10 @@ public class Autonomouse extends LinearOpMode {
     private DcMotor leftBack = null;
     private DcMotor rightFront = null;
     private DcMotor rightBack = null;
+
+    private DcMotor armMain = null;
+    //private Servo armClawL = null;
+    //private Servo armClawR = null;
 
     private void ctrlLeft(double pow) {
         leftFront.setPower(-pow);
@@ -29,6 +34,9 @@ public class Autonomouse extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
+        // Initialize state
+        int state = 0;
+
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
@@ -37,6 +45,10 @@ public class Autonomouse extends LinearOpMode {
         rightFront = hardwareMap.get(DcMotor.class, "right_front");
         rightBack = hardwareMap.get(DcMotor.class, "right_back");
 
+        armMain = hardwareMap.get(DcMotor.class, "arm_main");
+        //clawL = hardwareMap.get(Servo.class, "claw_left");
+        //clawR = hardwareMap.get(Servo.class, "claw_right");
+
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
@@ -44,18 +56,17 @@ public class Autonomouse extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPower = 1.0;
-
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
-
             // Send calculated power to wheels
-
-            ctrlLeft(0.5);
-            ctrlRight(0.5);
+            if (state == 0) {
+                ctrlLeft(0.5);
+                ctrlRight(0.5);
+                if (getRuntime() > 10) {
+                    state = 1;
+                }
+            } else if (state == 1) {
+                ctrlLeft(0);
+                ctrlRight(0);
+            }
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
